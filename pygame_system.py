853e8f681a339c78a665e4b429566f3187907103
nagga <c0a2302052@edu.teu.ac.jp>
2024-06-11 15:29:35 +0900

    ウィンドウの横幅変更

--- pygame_system.py
+++ pygame_system.py
@@ -7,7 +7,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
     pg.display.set_caption("はじめてのPygame")
-    screen = pg.display.set_mode((800, 600))
+    screen = pg.display.set_mode((800, 800))
     clock = pg.time.Clock()
     font = pg.font.Font(None, 80)
 
@@ -21,7 +21,6 @@ def main():
             if event.type == pg.QUIT: 
                 return
             
-        
         txt = font.render(str(tmr), True, (255, 255, 255))
         screen.fill((50, 50, 50))
         screen.blit(txt, [300, 200])
